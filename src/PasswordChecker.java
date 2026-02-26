2a3b6a1a370eb3d455f1a149b043149e8208bd85
Omkar Dashrath Bhoir <immortalomi14@gmail.com>
2026-02-26 14:49:54 +0530

    Test CI/CD

--- src/PasswordChecker.java
+++ src/PasswordChecker.java
@@ -12,7 +12,7 @@ public class PasswordChecker {
         if(password.matches(".*[0-9].*")) score++;
         if(password.matches(".*[!@#$%^&*()].*")) score++;
 
-        if(score <=1) return "WEAK PASSWORD";
+        if(score <=1) return "WEAK";
         else if(score ==2 || score==3) return "MEDIUM";
         else return "STRONG";
     }
